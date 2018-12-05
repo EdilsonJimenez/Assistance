b47de6ed0ba05c0b529ef5bb1dd33ce98237efbc
Edilson Jimenez <edilsonjimenezg@gmail.com>
2018-12-04 22:15:09 -0600

    Android

---
+++ app/src/main/java/com/example/edilson/assistance/RetrofitClient.java
@@ -0,0 +1,6 @@
+package com.example.edilson.assistance;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "http://localhost:8000/users/api/";
+
+}
---
+++ app/src/main/java/com/example/edilson/assistance/User.java
@@ -0,0 +1,5 @@
+package com.example.edilson.assistance;
+
+public class User {
+
+}
